ee882e9c5440bf2cf8f0d3930a97f628640097e7
michal <michal@michals-MacBook-Pro.local>
2021-11-10 19:06:20 +0100

    Return jobOffers list

--- app/services/job_offers/show.rb
+++ app/services/job_offers/show.rb
@@ -1,6 +1,17 @@
 module JobOffers
   class Show < ApplicationService
     def call
+      todays_offers = JobOffer.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+      highest_mean_salary.merge(offers: todays_offers)
+    end
+
+    private
+
+    def todays_offers_by_city(city)
+      JobOffer.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day, city: city)
+    end
+
+    def highest_mean_salary
       highest_mean_salary = {city: '', salary: 0}
       JobOffer::CITIES.each do |city|
         offers = todays_offers_by_city(city)
@@ -8,12 +19,6 @@ module JobOffers
         highest_mean_salary = {city: city, salary: mean_city_salary(offers)}
       end
       highest_mean_salary
-    end
-
-    private
-
-    def todays_offers_by_city(city)
-      JobOffer.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day, city: city)
     end
 
     def mean_city_salary(job_offers)
