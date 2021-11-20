fb9b898c60b002789d31ee0b60faa952f7879441
michal <michal@michals-MacBook-Pro.local>
2021-11-20 21:37:51 +0100

    Move query to model method

--- app/models/job_offer.rb
+++ app/models/job_offer.rb
@@ -1,6 +1,12 @@
 class JobOffer < ApplicationRecord
   validates :title, :salary, :city, presence: true
-  CITIES = %w(warszawa krakow wroclaw poznan trojmiasto katowice
-    slask lodz bialystok gdynia lublin rzeszow
-    bydgoszcz gliwice gliwice szczecin sopot)
+  CITIES = %w[warszawa krakow wroclaw poznan trojmiasto katowice
+              slask lodz bialystok gdynia lublin rzeszow
+              bydgoszcz gliwice gliwice szczecin sopot].freeze
+
+  def self.todays_offers(city = nil)
+    query = { created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day }
+    city.present? && query = query.merge({ city: city })
+    where(query)
+  end
 end
--- app/services/job_offers/index.rb
+++ app/services/job_offers/index.rb
@@ -7,27 +7,25 @@ module JobOffers
 
     private
 
-    def todays_offers_by_city(city)
-      JobOffer.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day, city: city)
-    end
-
     def highest_mean_salary
-      highest_mean_salary = {city: '', salary: 0}
+      highest_mean_salary = { city: '', salary: 0 }
       JobOffer::CITIES.each do |city|
-        offers = todays_offers_by_city(city)
+        offers = JobOffer.todays_offers(city)
         next if highest_mean_salary[:salary] > mean_city_salary(offers)
-        highest_mean_salary = {city: city, salary: mean_city_salary(offers)}
+
+        highest_mean_salary = { city: city, salary: mean_city_salary(offers) }
       end
       highest_mean_salary
     end
 
     def mean_city_salary(job_offers)
       return 0 if job_offers.empty?
+
       (job_offers.pluck(:salary).sum / job_offers.pluck.count.to_f).to_i
     end
 
     def offers
-      todays_offers = JobOffer.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+      todays_offers = JobOffer.todays_offers()
       todays_offers.ransack(company_or_title_or_city_cont: query).result
     end
   end
