7cf4f8ea4e1803e2295fb70cbeec69f958299419
michal <michal@michals-MacBook-Pro.local>
2021-11-28 13:22:16 +0100

    Calculte man_salary_for_each_seniority level

--- app/services/mean_salary/index.rb
+++ app/services/mean_salary/index.rb
@@ -7,18 +7,29 @@ module MeanSalary
         next if offers.size.zero?
 
         mean_salary = (offers.pluck(:salary).sum / offers.pluck.count.to_f).to_i
-        Salary.create(mean_salary: mean_salary, date: date)
+
+        Salary.create(mean_salary: mean_salary,
+                      date: date,
+                      junior_mean_salary: mean_salary_by_seniority_level(offers, 'junior'),
+                      mid_mean_salary: mean_salary_by_seniority_level(offers, 'mid'),
+                      senior_mean_salary: mean_salary_by_seniority_level(offers, 'senior'))
       end
     end
 
     private
+
+    def mean_salary_by_seniority_level(offers, seniority)
+      return if offers.where(seniority: seniority).empty?
+
+      (offers.where(seniority: seniority).pluck(:salary).sum / offers.where(seniority: seniority).pluck.count.to_f).to_i
+    end
 
     def calucale_from
       @calucale_from ||= Salary.last ? Salary.last.created_at : JobOffer.first.created_at
     end
 
     def days_to_calculate
-      (Time.zone.now - calucale_from).to_i / 86_400
+      ((Time.zone.now - calucale_from).to_i / 86_400) + 1
     end
   end
 end
